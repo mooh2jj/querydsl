a80ef17d8ae8535e6860796bd69014e239849663
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-10-11 12:16:58 +0900

    [ADD] paging test 간단하게 구현

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -167,4 +167,31 @@ public class QuerydslBasicTest {
 
     }
 
+    @Test
+    public void paging1() {
+        var results = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(results.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() {
+        var results = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(results.getTotal()).isEqualTo(4);
+        assertThat(results.getLimit()).isEqualTo(2);
+        assertThat(results.getOffset()).isEqualTo(1);
+        assertThat(results.getResults().size()).isEqualTo(2);
+    }
+
 }
