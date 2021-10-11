4225459d75217906668d48fff9be1d5924041d3e
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-10-11 16:38:03 +0900

    [ADD] @QueryProjection 활용

--- src/main/java/com/study/querydsl/dto/MemberDto.java
+++ src/main/java/com/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ public class MemberDto {
     private String username;
     private int age;
 
+    @QueryProjection        // Q객체로 만들어짐, but, 아키택쳐로서 방법이 좋지 않음
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
