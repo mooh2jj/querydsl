538b2076cb0f63554a2d8327207fcd25b158e502
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-15 16:21:01 +0900

    스프링 Data JPA 리포지토리로 변경 // basicTest error

---
+++ src/test/java/com/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.study.querydsl.repository;
+
+import com.study.querydsl.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    // TODO : error 뜲! ##고쳐야 할 부분!
+    @Test
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+    }
+
+}
